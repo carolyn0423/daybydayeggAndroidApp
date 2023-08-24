98d530a85ea7e076315ddfb95ae542c4724cda24
leslie leslie <leslie.jiang@hamels.com.tw>
2023-08-24 18:07:07 +0800

    20230824 @Leslie marge

--- app/src/main/java/com/hamels/daybydayegg/Main/View/ShoppingCartFragment.java
+++ app/src/main/java/com/hamels/daybydayegg/Main/View/ShoppingCartFragment.java
@@ -1,5 +1,6 @@
 package com.hamels.daybydayegg.Main.View;
 
+import android.os.Build;
 import android.os.Bundle;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -73,6 +74,10 @@ public class ShoppingCartFragment extends BaseFragment {
         setAppToolbarVisibility(true);
         ((MainActivity) getActivity()).bindWebView(webView);
         ((MainActivity) getActivity()).setMainIndexMessageUnreadVisibility(false);
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
+            webView.setWebContentsDebuggingEnabled(false); // 關閉調試模式以提高性能
+        }
 
         switch (orderType) {
             case "G":
